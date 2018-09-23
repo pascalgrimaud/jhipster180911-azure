4db0ac432e484d1bcd4ac2aa222775cc0d8b32b1
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-09-23 18:57:17 +0200

    Azure: work with vars

--- init_env.sh
+++ init_env.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 init_var() {
-    echo $1
-    echo $2
-    if [ $1 != "" ]; then return $1; fi
-    if [ $2 != "" ]; then return $2; fi
+    result=""
+    if [[ $1 != "" ]]; then
+        result=$1
+    elif [[ $2 != "" ]]; then
+        result=$2
+    fi
+    echo $result
 }
 
-$JH_TOTO=init_var($BUILD_REPOSITORY_URI, $TRAVIS_REPO_SLUG)
+JH_TOTO=$(init_var "$BUILD_REPOSITORY_URI" "$TRAVIS_REPO_SLUG")
 echo JH_TOTO=$JH_TOTO
