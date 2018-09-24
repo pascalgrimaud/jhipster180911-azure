cfd63ef339f358609aace646499e896202f7168e
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-09-24 06:44:00 +0200

    Azure: stop MySQL

--- test.sh
+++ test.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-source init_env.sh
+echo $(dirname $0)
+source $(dirname $0)/init_env.sh
 
-echo BUILD_REPOSITORY_URI=$BUILD_REPOSITORY_URI
-echo BUILD_REPOSITORY_URI="$BUILD_REPOSITORY_URI"
-echo BUILD_REPOSITORY_URI=$(BUILD_REPOSITORY_URI)
-echo "---"
-echo JH_REPO=$JH_REPO
-echo JH_REPO="$JH_REPO"
-echo JH_REPO=$(JH_REPO)
-echo JH_REPO="$(JH_REPO)"
-echo "---"
+# echo BUILD_REPOSITORY_URI=$BUILD_REPOSITORY_URI
+# echo BUILD_REPOSITORY_URI="$BUILD_REPOSITORY_URI"
+# echo BUILD_REPOSITORY_URI=$(BUILD_REPOSITORY_URI)
+# echo "---"
+# echo JH_REPO=$JH_REPO
+# echo JH_REPO="$JH_REPO"
+# echo JH_REPO=$(JH_REPO)
+# echo JH_REPO="$(JH_REPO)"
+# echo "---"
 echo JH_TOTO=$JH_TOTO
 echo JH_TOTO="$JH_TOTO"
-echo JH_TOTO=$(JH_TOTO)
-echo "---"
-JH_TEST=$BUILD_REPOSITORY_URI
-echo JH_TEST=$JH_TEST
-echo JH_TEST="$JH_TEST"
-echo JH_TEST=$(JH_TEST)
-echo JH_TEST="$(JH_TEST)"
+# echo JH_TOTO=$(JH_TOTO)
+# echo "---"
+# JH_TEST=$BUILD_REPOSITORY_URI
+# echo JH_TEST=$JH_TEST
+# echo JH_TEST="$JH_TEST"
+# echo JH_TEST=$(JH_TEST)
+# echo JH_TEST="$(JH_TEST)"
